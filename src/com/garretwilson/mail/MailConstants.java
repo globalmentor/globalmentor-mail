0a3b43dc637e842659cb42e06b85d2f1c5dbf21c
Garret Wilson <garret@globalmentor.com>
2003-02-02 01:40:48 +0000

    initial Java repository

---
+++ src/com/garretwilson/mail/MailConstants.java
@@ -0,0 +1,10 @@
+package com.garretwilson.mail;
+
+/**Constant values for mail.
+@author Garret Wilson
+*/
+public interface MailConstants
+{
+	/**The property that contains the mail host name.*/
+	public final String MAIL_HOST_PROPERTY="mail.host";
+}
