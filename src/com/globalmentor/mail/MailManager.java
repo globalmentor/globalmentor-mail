ae98984e2d955a9e4679d7fbcdf681ba45b59289
Garret Wilson <garret@globalmentor.com>
2008-04-18 23:20:31 +0000

    Integrated Guise mail configuration into new Guise supplementary application description facility.

--- src/com/globalmentor/mail/MailManager.java
+++ src/com/globalmentor/mail/MailManager.java
@@ -16,12 +16,13 @@
 
 package com.globalmentor.mail;
 
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.*;
 
 import javax.mail.*;
 
 import static com.globalmentor.mail.Mail.*;
+import static com.globalmentor.util.PropertiesUtilities.*;
 
 /**A class for managing mail with asynchronous sending.
 @author Garret Wilson
@@ -57,16 +58,16 @@ public class MailManager
 	The transport protocol should be specified in the {@value Mail#MAIL_TRANSPORT_PROTOCOL_PROPERTY} property.	
 	Besides the default JavaMail properties, this implementation expects the mail authentication user to be specified in the <code>mail.<var>transport</var>.user</code> property
 	and the mail authentication password to be specified in the <code>mail.<var>transport</var>.password</code> property
-	@param properties The JavaMail properties used to initialize the mail session.
+	@param properties The map of JavaMail properties used to initialize the mail session.
 	@exception NullPointerException if the given properties is <code>null</code>.
 	@exception NoSuchProviderException if the provider specified in the properties cannot be found.
 	*/
-	public MailManager(final Properties properties) throws NoSuchProviderException
+	public MailManager(final Map<?, ?> properties) throws NoSuchProviderException
 	{
-		session=Session.getInstance(properties);  //create a new mail session
-		final String transportProtocol=properties.getProperty(MAIL_TRANSPORT_PROTOCOL_PROPERTY);	//get the transport protocol
-		final String user=properties.getProperty(MAIL_PROTOCOL_USER_PROPERTY_TEMPLATE.apply(transportProtocol));	//get the user for the transport protocol
-		final String password=properties.getProperty(MAIL_PROTOCOL_PASSWORD_PROPERTY_TEMPLATE.apply(transportProtocol));	//get the password for the transport protocol
+		session=Session.getInstance(toProperties(properties));  //create a new mail session
+		final String transportProtocol=(String)properties.get(MAIL_TRANSPORT_PROTOCOL_PROPERTY);	//get the transport protocol
+		final String user=(String)properties.get(MAIL_PROTOCOL_USER_PROPERTY_TEMPLATE.apply(transportProtocol));	//get the user for the transport protocol
+		final String password=(String)properties.get(MAIL_PROTOCOL_PASSWORD_PROPERTY_TEMPLATE.apply(transportProtocol));	//get the password for the transport protocol
 		sendQueue=new LinkedBlockingQueue<Message>();	//create a new queue for sending mail messages
 		mailSender=new BlockingQueueMailSender(sendQueue, session.getTransport(), user, password);	//create the mail sender
 		sendThread=new Thread(mailSender, getClass().getName());	//create a new send thread
